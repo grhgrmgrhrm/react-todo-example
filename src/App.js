4be9b65a9f18abe7737709b943e377a158b68948
grhgrmgrhrm <grhgrmgrhrm@gmail.com>
2021-09-08 23:10:50 +0300

    init: add app files to git

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import ToDo from "./components/ToDo";
+
+export default function App() {
+  console.log("%c Обнаружил ошибку? \n Давай обсудим приложуху на unicoding.dev/grhgrmgrhrm",
+      "background-color: #0053b3; color: #fff; ");
+  return <ToDo />;
+}
